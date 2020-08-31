df6decb3afb6075507e124e1a6cc98b692a7d25d
Mr-Steal-Your-Script <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-08-30 21:53:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import time
+import praw
+import random
+import config
+import pyimgur
+from halo import Halo
+
+spinner = Halo(spinner={'interval': 100, 'frames': ['-', '+', '*', '+', '-']})
+#spinner.start()
+
+reddit = praw.Reddit(client_id=config.C_ID, client_secret=config.C_S, user_agent=config.U_A, username=config.UN, password=config.PW)
+imgur_id = config.I_ID
+
+while True:
+    try:
+        spinner.start()
+        subreddit = reddit.subreddit('random')
+        domains = ['i.redd.it', 'i.imgur.com']
+        limit = None
+        print('Random Subreddit Is: ', subreddit)
+        
+        submissions = list(subreddit.top('all', limit=limit))
+        submission = random.choice(submissions)
+        if submission.domain in domains:
+            im = pyimgur.Imgur(imgur_id)
+            uploaded_image = im.upload_image(url=submission.url)
+            with open ('links.txt', "a") as f:
+                f.write(uploaded_image.link + "\n")
+            reddit.validate_on_submit = True
+            subreddit.submit(submission.title, url=uploaded_image.link)
+            spinner.succeed('success')
+            
+        elif submission.domain not in domains:
+            spinner.info('domain is not in domains :(')
+            
+    except Exception as e:
+        exc = str(str(e))
+        spinner.fail(text=exc)
+        time.sleep(60)
+        
+    except KeyboardInterrupt:
+        spinner.warn(text='shutting down :(')
+        quit()
