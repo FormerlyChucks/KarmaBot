4fe593fc1f00d326ed12d9f1c97e62672a6d70af
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-10-09 18:46:10 -0500

    Update main.py

--- new/main.py
+++ new/main.py
@@ -1,4 +1,3 @@
- 
 import time
 import praw
 import random
@@ -6,15 +5,15 @@ import config
 import pyimgur
 
 reddit = praw.Reddit(client_id=config.C_ID, client_secret=config.C_S, user_agent=config.U_A, username=config.UN, password=config.PW)
+subreddit = reddit.subreddit(config.sub)
 imgur_id = config.I_ID
-
+domains = ['i.redd.it', 'i.imgur.com']
+limit = None
+x = random.randint(0,2)
 while True:
     try:
         subreddit = reddit.subreddit('random')
-        domains = ['i.redd.it', 'i.imgur.com']
-        limit = None
-        print('Random Subreddit Is: ', subreddit)
-        
+        print('Random Subreddit Is: ', subreddit)       
         submissions = list(subreddit.top('all', limit=limit))
         submission = random.choice(submissions)
         if submission.domain in domains:
@@ -24,10 +23,17 @@ while True:
                 f.write(uploaded_image.link + "\n")
             reddit.validate_on_submit = True
             subreddit.submit(submission.title, url=uploaded_image.link)
-            print('success')
-            
+            print('success')          
         elif submission.domain not in domains:
-            print('domain is not in domains :(')
+            print('domain is not in domains :(')      
+        for submission in subreddit.stream.submissions(skip_existing=True):
+            for results in subreddit.search(submission.title): 
+                similarity = difflib.SequenceMatcher(None, submission.title,results.title).ratio()
+                if results.num_comments >= 3 and similarity >= .8:
+                    comment = results.comments[x]
+                    if comment.author not in config.usernames and comment.body != '[deleted]':
+                        submission.reply(comment.body)
+                        break
             
     except Exception as e:
         print(e)
