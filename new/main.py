04ef29801d115ceac45a3d523c95fbe26c33dbaf
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-11-05 13:20:16 -0600

    Update main.py

--- new/main.py
+++ new/main.py
@@ -2,7 +2,6 @@ import time, praw, random, config, pyimgur, traceback
 
 reddit = praw.Reddit(client_id=config.C_ID, client_secret=config.C_S, user_agent=config.U_A, username=config.UN, password=config.PW)
 reddit.validate_on_submit = True
-x = random.randint(0,2)
 
 while True:
     try:
@@ -17,7 +16,7 @@ while True:
             for results in subreddit.search(submission.title): 
                 similarity = difflib.SequenceMatcher(None, submission.title,results.title).ratio()
                 if results.num_comments >= 3 and similarity >= .8:
-                    comment = results.comments[x]
+                    comment = results.comments[random.randint(0,2)]
                     if comment.author not in config.usernames and comment.body != '[deleted]':
                         submission.reply(comment.body)
                         break           
