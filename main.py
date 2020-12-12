03e5cb8dc69e8209b8861e7b6ffac8b8cd846e64
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2020-12-11 23:39:08 -0600

    #1

--- main.py
+++ main.py
@@ -9,18 +9,29 @@ with open("config.yaml") as config_file:
     password = config["password"]
     imgur_id = config["imgur_id"]
 
-reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent, username=username, password=password)
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent,
+                     username=username,password=password)
+
+reddit.validate_on_submit = True
 
 while True:
     try:
         subreddit = reddit.subreddit('random')
         print('Random Subreddit Is: ', subreddit)
-        submission = random.choice(list(subreddit.top('all', limit=None)))
+        submissions = list(subreddit.top('all', limit=None))
+        submission = random.choice(random.choice(submissions))
         if submission.domain == 'i.redd.it':
-            uploaded_image = pyimgur.Imgur(imgur_id).upload_image(url=submission.url)
-            reddit.validate_on_submit = True
+            im = pyimgur.Imgur(imgur_id)
+            im.upload_image(url=submission.url)
             subreddit.submit(submission.title, url=uploaded_image.link)
             print('success')
-        else: print('domain is not i.redd.it')
-    except Exception: print(traceback.format_exc());time.sleep(60)
-    except KeyboardInterrupt: print('Shutting Down :(');quit()
+        else:
+            print('domain is not i.redd.it')
+    except Exception:
+        print(traceback.format_exc())
+        time.sleep(60)
+    except KeyboardInterrupt:
+        print('Shutting Down :(')
+        quit()
