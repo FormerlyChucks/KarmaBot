c864b71ae51846e0acd1d684fc1fbbdc646fd6ab
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-01-29 07:14:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ with open("config.yaml") as config_file:
 reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=user_agent,
-                     username=username,password=password)
+                     username=username,
+                     password=password)
 
 reddit.validate_on_submit = True
 
@@ -21,9 +22,9 @@ while True:
         subreddit = reddit.subreddit('random')
         print('Random Subreddit Is: ', subreddit)
         submissions = list(subreddit.top('all', limit=None))
-        submission = random.choice(random.choice(submissions))
+        submission = random.choice(submissions)
         if submission.domain == 'i.redd.it':
-            im = pyimgur.Imgur(imgur_id)
+            uploaded_image = im = pyimgur.Imgur(imgur_id)
             im.upload_image(url=submission.url)
             subreddit.submit(submission.title, url=uploaded_image.link)
             print('success')
