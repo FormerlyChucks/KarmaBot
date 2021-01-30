a16dc6d4d8d155e61eb560e1fd4f791dec41d59f
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-01-29 23:34:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,24 @@ import time, praw, yaml, random, pyimgur, traceback
 
 with open("config.yaml") as config_file:
     config = yaml.safe_load(config_file)
-    client_id = config["client_id"]
-    client_secret = config["client_secret"]
-    user_agent = config["user_agent"]
-    username = config["username"]
-    password = config["password"]
-    imgur_id = config["imgur_id"]
 
-reddit = praw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     user_agent=user_agent,
-                     username=username,
-                     password=password)
+reddit = praw.Reddit(client_id=config["client_id"],
+                     client_secret=config["client_secret"],
+                     user_agent=config["user_agent"],
+                     username=config["username"],
+                     password=config["password"])
 
 reddit.validate_on_submit = True
+im = pyimgur.Imgur(config["imgur_id"])
 
 while True:
     try:
         subreddit = reddit.subreddit('random')
-        print('Random Subreddit Is: ', subreddit)
+        print('Random Subreddit Is: ',subreddit)
         submissions = list(subreddit.top('all', limit=None))
         submission = random.choice(submissions)
         if submission.domain == 'i.redd.it':
-            uploaded_image = im = pyimgur.Imgur(imgur_id)
-            im.upload_image(url=submission.url)
+            uploaded_image = im.upload_image(url=submission.url)
             subreddit.submit(submission.title, url=uploaded_image.link)
             print('success')
         else:
