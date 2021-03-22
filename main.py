90cf0c7062fa37eb61b8b8beef437db537ae4c4c
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-22 13:36:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,25 @@ reddit = praw.Reddit(client_id=config["client_id"],
 
 reddit.validate_on_submit = True
 
+def submit(title,url):
+    subreddit.submit(title=title, url=pyimgur.Imgur(config["imgur_id"]).upload_image(url=submission.url).link)
+    
+
 while True:
     try:
-        subreddit = reddit.subreddit('random');print('Random Subreddit Is: ',subreddit)
-        submissions = list(subreddit.top('all', limit=None));submission = random.choice(submissions)
-        if submission.domain == 'i.redd.it': subreddit.submit(submission.title, url=pyimgur.Imgur(config["imgur_id"]).upload_image(url=submission.url).link);print('success')
-        else: print('domain is not i.redd.it')
-    except Exception: print(traceback.format_exc());time.sleep(60)
-    except KeyboardInterrupt: print('Shutting Down :(');quit()
+        subreddit = reddit.subreddit('random')
+        print('Random Subreddit Is: ',subreddit)
+        submissions = list(subreddit.top('all', limit=None))
+        submission = random.choice(submissions)
+        if submission.domain == 'i.redd.it':
+            link = pyimgur.Imgur(config["imgur_id"]).upload_image(url=submission.url).link
+            subreddit.submit(title, url=link)
+            print('success')
+        else:
+            print('domain is not i.redd.it')
+    except Exception:
+        print(traceback.format_exc())
+        time.sleep(60)
+    except KeyboardInterrupt:
+        print('Shutting Down :(')
+        quit()
