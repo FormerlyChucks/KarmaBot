93484a75f2b7dffe7a5505b3e37432b5b453e075
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-06-21 14:01:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,20 @@
-import time, praw, yaml, random, pyimgur, traceback
+import praw, yaml, random, pyimgur
 
 with open("config.yaml") as cf:
     config = yaml.safe_load(cf)
-    _id = cf["client_id"]
-    secret = cf["client_secret"]
-    agent = cf["user_agent"]
-    username = cf["username"]
-    password = cf["password"]
-    imgur_key = cf['imgur_id']
 
-reddit = praw.Reddit(client_id=_id,
-                     client_secret=secret,
-                     user_agent=agent,
-                     username=username,
-                     password=password)
+reddit = praw.Reddit(client_id=cf["client_id"],
+                     client_secret=cf["client_secret"],
+                     user_agent=cf["user_agent"],
+                     username=cf["username"],
+                     password=cf["password"])
 
 reddit.validate_on_submit = True
-
-def upload(imgur_id,url):
-    img = pyimgur.Imgur(imgur_id)
-    image = img.upload_image(url=url)
-    return image.link
-    
-def submit(title,url):
-    subreddit.submit(title,url)
-    return 'success'
-
-while True:
-    try:
-        subreddit = reddit.subreddit('random')
-        print('Random Subreddit Is: ',subreddit)
-        submissions = list(subreddit.top('all', limit=None))
-        submission = random.choice(submissions)
-        if submission.domain == 'i.redd.it':
-            link = upload(imgur_id=imgur_key)
-            post = submit(title, url=link)
-            print(post)
-        else:
-            print('domain is not i.redd.it')
-    except Exception:
-        print(traceback.format_exc())
-        time.sleep(60)
-    except KeyboardInterrupt:
-        print('Shutting Down :(')
-        quit()
+img = pyimgur.Imgur(cf['imgur_id'])
+subreddit = reddit.subreddit('random')
+print('Random Subreddit Is:',subreddit.display_name)
+post = random.choice([submission for submission in list(subreddit.top('all', limit=None)) if submission.domain == 'i.redd.it'])
+print('Random Submission from /r/'+subreddit.display_name,'is:',post.id)
+image = img.upload_image(url=post.url)
+submit = subreddit.submit(title,url=image.link)
+print('Repost located here:',submit.url)
