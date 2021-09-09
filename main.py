a28caf1ec94351257e764b6d22ca2ec94cd5aade
dio <61555147+FormerlyChucks@users.noreply.github.com>
2021-09-08 21:17:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import praw, yaml, random, pyimgur
 with open("config.yaml") as cf:
     config = yaml.safe_load(cf)
 
-reddit = praw.Reddit(client_id=cf["client_id"],
-                     client_secret=cf["client_secret"],
-                     user_agent=cf["user_agent"],
-                     username=cf["username"],
-                     password=cf["password"])
+reddit = praw.Reddit(client_id=config["client_id"],
+                     client_secret=config["client_secret"],
+                     user_agent=config["user_agent"],
+                     username=config["username"],
+                     password=config["password"])
 
 reddit.validate_on_submit = True
-img = pyimgur.Imgur(cf['imgur_id'])
+img = pyimgur.Imgur(config['imgur_id'])
 subreddit = reddit.subreddit('random')
 print('Random Subreddit Is:',subreddit.display_name)
 post = random.choice([submission for submission in list(subreddit.top('all', limit=None)) if submission.domain == 'i.redd.it'])
